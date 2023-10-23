6965acc80222bfff96cb196a09aabb2f150d47c5
kitmyfaceplease <kitmyfaceplease2@gmail.com>
2023-10-23 11:23:21 +0800

    初始化提交

---
+++ app.js
@@ -0,0 +1,55 @@
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+//在app.js中设置public文件夹为静态文件夹
+app.use(express.static('public'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+app.post('/place', async (req, res) => {
+    const placeName = req.body.placeName;
+    // 检查place参数是否有效
+    if (!placeName || placeName.trim() === '') {
+        const error = new Error('Invalid place parameter.');
+        return next(error);
+    }
+
+    const GAODE_API_KEY = 'b6ba147ffd1e49158d12f7cb16d0f381';
+    const GAODE_GEOCODE_URL = `https://restapi.amap.com/v3/geocode/geo?address=${encodeURIComponent(placeName)}&key=${GAODE_API_KEY}`;
+
+    let location = null; // 定义location变量在try块之前
+
+    try {
+        const response = await axios.get(GAODE_GEOCODE_URL);
+        if (response.data && response.data.geocodes && response.data.geocodes.length > 0) {
+            location = response.data.geocodes[0].location;
+        } else {
+            throw new Error('Unable to geocode the provided place.');
+        }
+    } catch (error) {
+        return next(error); // 使用next()将错误传递给下一个中间件
+    }
+    
+    if (location) {
+        const [longitude, latitude] = location.split(',');
+        res.render('map', { latitude: latitude, longitude: longitude });
+    } else {
+        res.send(`Unable to find location for ${placeName}`);
+    }
+});
+
+//在app.js的底部，添加以下代码来创建一个错误处理中间件：
+app.use((err, req, res, next) => {
+    console.error(err.stack); // 打印错误堆栈到控制台
+    res.status(500).send('Something went wrong! ' + err.message);
+});
